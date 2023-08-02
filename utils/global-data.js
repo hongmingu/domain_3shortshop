4262204b620cb2f6e2d31991b367ff09a06eef07
hongmingu <gmh397@nate.com>
2023-08-02 17:58:49 +0900

    40

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : '공부의 달인';
+    : '프로그래밍과 교과수업 길잡이';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : '공부, 학습의 선두주자 정보방출';
+    : '어떻게 하면 잘 학습할 수 있을지에 대한 정보 기록';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
